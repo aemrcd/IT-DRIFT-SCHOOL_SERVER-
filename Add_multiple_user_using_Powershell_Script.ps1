6b70127359702ad1d91b185dc4658be5e59e95cd
aemrcd <154060630+aemrcd@users.noreply.github.com>
2025-03-24 01:20:55 -0700

    Add files via upload
    Added The Powershell script for Multiple user

---
+++ Add_multiple_user_using_Powershell_Script.ps1
@@ -0,0 +1,19 @@
+﻿Import-Module ActiveDirectory
+
+$csvfile = "<File Location of the INFO.csv> "
+
+$users = import-Csv -Path $csvfile
+
+foreach($user in $users){
+$FirstName = $user.Name
+$GivenName = $user.GivenName
+$LastName = $user.Surname
+$DisplayName = $user.DisplayName
+$Username = $user.SAMAccountName
+$Password = $user.Password
+$Description = $user.Description
+$OU = $user.OU
+
+New-ADUser -Name "$FirstName $LastName" `-GivenName $GivenName `-Surname $LastName `-DisplayName $DisplayName `-SamAccountName "$Username" `-AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) `-Description $Description `-Path $OU `-Enabled $true `-PassThru
+
+}
